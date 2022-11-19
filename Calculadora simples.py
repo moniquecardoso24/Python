c53cd6369d7d217d82a11bbf6d37ab86567f7b16
moniquecardoso24 <118371689+moniquecardoso24@users.noreply.github.com>
2022-11-18 22:27:17 -0300

    Criando uma calculadora

---
+++ Calculadora simples.py
@@ -0,0 +1,29 @@
+while True:
+    print()
+    num_1 = input('Digite um número: ')
+    num_2 = input('Digite outro número: ')
+    operador = input('Digite um operador: ')  # + , - , / , *
+    sair = input('Deseja sair? [s]im ou [n]ão: ')
+
+    if sair == 's':
+        break
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('É necessário digitar um número!')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    else:
+        print('Operador inválido!')
+
+
